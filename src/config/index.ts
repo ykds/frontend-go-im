c86bfd87d2910b6a070e5ec7fb9402bd2c959440
ykds <conamorek@163.com>
2025-05-06 16:57:18 +0800

    update

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,3 +1,3 @@
-export const API_BASE_URL = 'http://localhost:8003'
-export const SEQ_URL = 'http://localhost:8004'
-export const WS_URL = 'http://localhost:8002'
+export const API_BASE_URL = 'http://localhost:9000'
+export const SEQ_URL = 'http://localhost:9001'
+export const WS_URL = 'http://localhost:8012'
